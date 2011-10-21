2972b00886d08e41380d446a5a76de58681cac9a
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-10-21 13:29:07 +0100

    Added basic serving of content

--- django_jumpstart_project/django_jumpstart_app/views.py
+++ django_jumpstart_project/django_jumpstart_app/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.shortcuts  import render_to_response, get_object_or_404, redirect
+from django.template   import RequestContext
+
+# from django.views.generic.list_detail import object_detail, object_list
+
+from django_jumpstart_app import models
+
+def home(request):
+    """Homepage"""
+    return render_to_response(
+        'index.html',
+        {},
+        context_instance=RequestContext(request)
+    )
+
--- django_jumpstart_project/urls.py
+++ django_jumpstart_project/urls.py
@@ -2,19 +2,24 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.defaults import patterns, include, url
 
+# Admin section
 from django.contrib import admin
 admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_jumpstart_project.views.home', name='home'),
-    # url(r'^django_jumpstart_project/', include('django_jumpstart_project.foo.urls')),
-
-    # admin
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
-# serve media_root files (only works when DEBUG is True)
+# Our apps
+urlpatterns += patterns('django_jumpstart_app.views',
+    url( r'^$', 'home', name='home' ),
+)
+
+# Accounts
+urlpatterns += patterns('',
+    (r'^accounts/', include('registration.backends.default.urls')),
+)
+
+# serve media_root files (only works when settings.DEBUG is True)
 # https://docs.djangoproject.com/en/1.3/howto/static-files/#django.conf.urls.static.static
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

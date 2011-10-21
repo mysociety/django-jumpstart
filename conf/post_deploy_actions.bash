e3d1580e178ea8ed0a3d51a1d3aaf0aaa8bfa8b9
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-10-21 11:53:36 +0100

    mysoc deploy instructions, and post deploy script

---
+++ conf/post_deploy_actions.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# abort on any errors
+set -e
+
+# create/update the virtual environment
+pip install \
+    -E ../django-jumpstart-virtualenv \
+    -r conf/requirements.txt
+
+# use the virtualenv just created/updated
+source ../django-jumpstart-virtualenv/bin/activate
+
+
+# go to the project directory for local config
+cd ./django_jumpstart_project
+
+# get the database up to speed
+./manage.py syncdb
+./manage.py migrate
+
+# gather all the static files in one place
+./manage.py collectstatic --noinput
+
+
+cd --
+

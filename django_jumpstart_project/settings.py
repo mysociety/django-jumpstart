2f56ecac27e68639e59ac5094d7fad880b04c838
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-10-21 12:18:54 +0100

    Fix a few copy-and-paste errors

--- django_jumpstart_project/settings.py
+++ django_jumpstart_project/settings.py
@@ -135,13 +135,13 @@ MIDDLEWARE_CLASSES = (
     'pagination.middleware.PaginationMiddleware',
 )
 
-ROOT_URLCONF = 'mzalendo.urls'
+ROOT_URLCONF = 'django_jumpstart_project.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join( base_dir, "pylib/mzalendo/templates" ),
+    os.path.join( base_dir, "django_jumpstart_project/templates" ),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (

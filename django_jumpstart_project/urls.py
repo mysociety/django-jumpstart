ef1c19edfcf31bed393a4e456eb316fb5f486bab
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-10-21 12:23:47 +0100

    Added sample admin and enabled autodiscovery

--- django_jumpstart_project/urls.py
+++ django_jumpstart_project/urls.py
@@ -2,20 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_jumpstart_project.views.home', name='home'),
     # url(r'^django_jumpstart_project/', include('django_jumpstart_project.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # admin
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 # serve media_root files (only works when DEBUG is True)

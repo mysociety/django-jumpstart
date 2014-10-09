f8968df6779d375358767115a2fa1d50f9753165
Matthew Somerville <matthew@mysociety.org>
2014-10-09 10:03:04 +0100

    Only create a virtualenv if not already there.
    Update documentation on virtualenv system argument, and make
    sure pip is upgraded if necessary.

--- project_template/conf/post_deploy_actions.bash
+++ project_template/conf/post_deploy_actions.bash
@@ -9,9 +9,22 @@ cd `dirname $0`/..
 # create/update the virtual environment
 # NOTE: some packages are difficult to install if they are not site packages,
 # for example xapian. If using these you might want to add the
-# '--enable-site-packages' argument.
-virtualenv ../virtualenv-{{ project_name }}
-source ../virtualenv-{{ project_name }}/bin/activate
+# '--system-site-packages' argument.
+virtualenv_args=""
+virtualenv_dir='../virtualenv-{{ project_name }}'
+virtualenv_activate="$virtualenv_dir/bin/activate"
+if [ ! -f "$virtualenv_activate" ]
+then
+    virtualenv $virtualenv_args $virtualenv_dir
+fi
+source $virtualenv_activate
+
+# Upgrade pip to a secure version
+pip_version="$(pip --version)"
+if [ "$(echo -e 'pip 1.4\n'"$pip_version" | sort -V | head -1)" = "$pip_version" ]; then
+    curl -L -s https://bootstrap.pypa.io/get-pip.py | python
+fi
+
 pip install --requirement requirements.txt
 
 # make sure that there is no old code (the .py files may have been git deleted)
